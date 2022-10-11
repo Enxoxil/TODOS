f79067783d7045f29888b174d492b230e4bd698c
Yehor Haponov <egoriks1994@gmail.com>
2022-10-11 22:53:20 +0300

    Fix: Add AntDesign components

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@
 import './App.css';
 import Todos from "./components/Todos";
 import NewTodo from "./components/NewTodo";
-
 import TodosContextProvider from "./store/todos-context";
+import _PageHeader from "./components/PageHeader/_PageHeader";
 
 
 function App() {
 
     return (
         <TodosContextProvider>
+            <_PageHeader/>
             <NewTodo />
             <Todos />
         </TodosContextProvider>
