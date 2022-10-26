85a42db0a96a457489c6846e129bd4805b480c37
Yehor Haponov <egoriks1994@gmail.com>
2022-10-26 23:57:31 +0300

    feat: create new branch 'router'

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from 'react';
+import {NavLink} from "react-router-dom";
+
+const Navigation: FC = (props) => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><NavLink to='/home'>Home</NavLink></li>
+                    <li><NavLink to='/todos'>Todos</NavLink></li>
+                    <li><NavLink to='/admin'>Admin</NavLink></li>
+                </ul>
+            </nav>
+        </>
+    )
+};
+
+export default Navigation;
