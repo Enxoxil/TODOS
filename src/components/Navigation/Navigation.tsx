fabc3286b57415492a6428a0520f7ec3b70ac581
Yehor Haponov <egoriks1994@gmail.com>
2022-10-31 23:53:46 +0200

    refactor: fix CSS bugs

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -13,9 +13,9 @@ const Navigation: FC<INavigation> = ({user, handleLogout, handleLogin}) => {
     return (
             <nav className={classes.todoNavigate}>
                 <ul className={classes.todoNavigate__Wrapper}>
-                    <li className={classes.todoNavigate__itemWrapper}><NavLink to='/home' className={classes.todoNavigate__item}>Home</NavLink></li>
-                    <li className={classes.todoNavigate__itemWrapper}><NavLink to='/todos' className={classes.todoNavigate__item}>Todos</NavLink></li>
-                    <li className={classes.todoNavigate__itemWrapper}><NavLink to='/admin' className={classes.todoNavigate__item}>Admin</NavLink></li>
+                    <li className={classes.todoNavigate__itemWrapper}><NavLink to='/home' tabIndex={0} className={classes.todoNavigate__item}>Home</NavLink></li>
+                    <li className={classes.todoNavigate__itemWrapper}><NavLink to='/todos' tabIndex={0} className={classes.todoNavigate__item}>Todos</NavLink></li>
+                    <li className={classes.todoNavigate__itemWrapper}><NavLink to='/admin' tabIndex={0} className={classes.todoNavigate__item}>Admin</NavLink></li>
                     <li className={classes.todoNavigate__itemWrapper}>
                         {user! && user!.login ? (
                             <button className={classes.todoNavigate__item} onClick={handleLogout}>Sign out</button>
